3bd266e7d25aae09387db03572963e11eb2468f9
ONEO <oneo.me@outlook.com>
2019-08-07 17:19:45 +0800

    兼容 Edge

--- src/background.js
+++ src/background.js
@@ -1,18 +1,21 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-    var url = prompt("设置以下链接为默认标签页？", tab.url)
-    if (url != null) {
-        localStorage.url = url
-    }
+chrome.browserAction.onClicked.addListener(function(tab) {
+	var url = prompt('设置以下链接为默认标签页？', tab.url)
+	if (url != null) {
+		localStorage.url = url
+	}
 })
 
 chrome.tabs.onCreated.addListener(onCreated)
 
 function onCreated(tab) {
-    if (tab.url == "chrome://newtab/") {
-        chrome.tabs.update(tab.id, { url: localStorage.url || "https://www.google.com", selected: true })
-    }
+	if (tab.url === 'chrome://newtab/' || tab.url === 'edge://newtab/') {
+		chrome.tabs.update(tab.id, {
+			url: localStorage.url || 'https://www.google.com',
+			selected: true
+		})
+	}
 }
 
-chrome.tabs.query({}, function (tabs) {
-    tabs.forEach(onCreated)
+chrome.tabs.query({}, function(tabs) {
+	tabs.forEach(onCreated)
 })
